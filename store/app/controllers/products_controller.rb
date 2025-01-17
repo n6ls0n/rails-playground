3cc36ec7b074c4d18df3c73ffeb1435874a29caa
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2025-01-17 13:35:47 -0330

    Added update action

--- store/app/controllers/products_controller.rb
+++ store/app/controllers/products_controller.rb
@@ -24,6 +24,15 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def product_params
       params.expect(product: [ :name ])
