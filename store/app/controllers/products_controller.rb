e17321130bc32b23f01c773b09a3a3f6c694471c
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2025-01-17 21:43:46 -0330

    Refactored create action

--- store/app/controllers/products_controller.rb
+++ store/app/controllers/products_controller.rb
@@ -15,7 +15,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    if product.after_save
+    if @product.save
       redirect_to @product
     else
       render :new, status: :unprocessable_entity
